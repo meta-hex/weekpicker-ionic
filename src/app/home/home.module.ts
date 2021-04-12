b663511cb992b29d18d77253523c1f4c70cd2961
sr-spark <cn.bitmain@gmail.com>
2021-04-12 12:05:32 +0800

    v1

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { WheelSelector } from '@ionic-native/wheel-selector/ngx';
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage],
+  providers: [
+    WheelSelector
+  ]
+})
+export class HomePageModule {}
